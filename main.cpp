f351d5fb01a67bcc40ede0f70698dd8968907b32
Vitaliy <vigleg777@gmail.com>
2024-10-15 16:20:51 +0000

    hot fix 1.0

--- main.cpp
+++ main.cpp
@@ -7,23 +7,3 @@ int main(int argc, char *argv[])
     fire->show();
     return app.exec();
 }
-
-
-/* CREATE TABLE employees (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    full_name TEXT NOT NULL,
-    password TEXT NOT NULL
-    );
-
-CREATE TABLE reports (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    employee_id INTEGER,
-    temperature REAL NOT NULL,
-    wind_speed REAL NOT NULL,
-    weather TEXT NOT NULL,
-    report_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (employee_id) REFERENCES employees (id)
-    )
-
-
-INSERT INTO employees (full_name, password) VALUES ('Имя Фамилия', 'ваш_пароль');*/
